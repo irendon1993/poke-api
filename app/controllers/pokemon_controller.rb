3dc07f6bafbf320740a5f400177bdc709269599f
irendon1993 <isaacrendon7@gmail.com>
2022-04-29 13:41:33 -0400

    Hit new endpoints wiht more data

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -2,7 +2,7 @@ class PokemonController < ApplicationController
   def index
     pokemon = params[:pokemon]
     conn = Faraday.new("https://pokeapi.co")
-    response = conn.get("/api/v2/pokemon-form/#{pokemon}/")
+    response = conn.get("/api/v2/pokemon/#{pokemon}/")
     @pokemon = JSON.parse(response.body, symbolize_names: true)
   end
 end
