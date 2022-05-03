890a8746980be9b9db4416d808ae2fc04ece7af7
irendon1993 <isaacrendon7@gmail.com>
2022-05-03 13:01:04 -0400

    popualted db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'json'
+require 'open-uri'
+
+poke_ids = (1..151).to_a
+
+151.times do |index|
+  poke_id = poke_ids[index]
+  conn = Faraday.new("https://pokeapi.co")
+  response = conn.get("/api/v2/pokemon/#{poke_id}/")
+  @pokemonApi = JSON.parse(response.body, symbolize_names: true)
+  new_poke = Pokemon.create(
+    pokeid: @pokemonApi[:id],
+    name: @pokemonApi[:name],
+    iamgeurl: @pokemonApi[:sprites][:front_default],
+    # type: @pokemonApi[:types].first[:type][:name].capitalize
+  )
+end
